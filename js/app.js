3714685a8a3c203bc90e3459d762f35c487dae3a
Marco Rezende <marcorezende@outlook.com.br>
2018-11-21 20:43:22 -0200

    Adicionando função criadora de elementos "li"

--- js/app.js
+++ js/app.js
@@ -1,1 +1,9 @@
-const cardList = ["fa-diamond", "fa-paper-plane-o", "fa-anchor", "fa-bolt", "fa-cube", "fa-leaf", "fa-bicycle", "fa-bomb"];
+const cardList = ["fa-birthday-cake", "fa-angry", "fa-anchor", "fa-bolt", "fa-cube", "fa-leaf", "fa-bicycle", "fa-bomb"];
+
+
+function createCard(cardClass){
+    $("ul.grid").append(`<li class="card"><i class="fas"></i></li>`);
+}
+
+// Iniciador
+createCard()
