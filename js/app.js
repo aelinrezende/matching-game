1ab0132cb91e63d1f313aa85c469b180918f6453
Marco Rezende <marcorezende@outlook.com.br>
2018-11-21 20:54:08 -0200

    Adicionando shuffler

--- js/app.js
+++ js/app.js
@@ -1,9 +1,25 @@
 const cardList = ["fa-birthday-cake", "fa-angry", "fa-anchor", "fa-bolt", "fa-cube", "fa-leaf", "fa-bicycle", "fa-bomb"];
 
+function shuffle(array) {
+    let currentIndex = array.length, temporaryValue, randomIndex;
+    while (currentIndex !== 0) {
+        randomIndex = Math.floor(Math.random() * currentIndex);
+        currentIndex -= 1;
+        temporaryValue = array[currentIndex];
+        array[currentIndex] = array[randomIndex];
+        array[randomIndex] = temporaryValue;
+    }
+    return array;
+}
 
 function createCard(cardClass){
-    $("ul.grid").append(`<li class="card"><i class="fas"></i></li>`);
+    $("ul.grid").append(`<li class="card"><i class="fa ${cardClass}"></i></li>`);
+}
+
+function positioner(){
+    shuffle(cardList).forEach(createCard);
 }
 
 // Iniciador
-createCard()
+positioner()
+positioner()
