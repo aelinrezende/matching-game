7661ba161e94dc78754c5ae9e727adb29e43eb78
Marco Rezende <marcorezende@outlook.com.br>
2018-11-26 16:50:17 -0200

    Reorganizando o código JS

--- js/app.js
+++ js/app.js
@@ -1,12 +1,14 @@
 const cardList = ["fa-birthday-cake", "fa-send", "fa-anchor", "fa-bolt", "fa-cube", "fa-leaf", "fa-bicycle", "fa-bomb"];
-let openCard = false;
-let moves = 0;
-let opened = [], starsOut = 3;
-let timer, timerFinish = false, duration, seconds, minutes, display;
+let timer, duration, seconds, minutes;
+let moves = 0, starsOut = 3;
+let timerFinish = false
+let opened = []
+
 
 // Iniciador
+start()
 
-
+// Função que executa o modal inicial e outras funções para dar inicio ao jogo
 function start() {
 	swal({
 		title: "Welcome to the 'Memory Game'",
@@ -15,17 +17,16 @@ function start() {
 		closeOnClickOutside: false,
 		allowEscapeKey: false,
 	}).then((normalMode) => {
-	if (normalMode) {
-		$( ".container" ).css('opacity', '1')
-		callPositioner()
-		verifyClick()
-		countdown()
-  }
-})
+		if (normalMode) {
+			$( ".container" ).css('opacity', '1')
+			callPositioner()
+			verifyClick()
+			countdown()
+	  	}
+	})
 };
 
-start()
-
+// Função que randomiza os cards
 function shuffle(array) {
     let currentIndex = array.length, temporaryValue, randomIndex;
     while (currentIndex !== 0) {
@@ -38,22 +39,51 @@ function shuffle(array) {
     return array;
 }
 
+// Criando cards a partir de um icone e o posicionando como ultimo filho do "grid"
 function createCard(cardClass) {
     $("ul.grid").append(`<li class="card"><i class="fa ${cardClass}"></i></li>`);
 }
 
+// Passando para função createCard um novo icone, para que se crie um novo card
 function positioner(){
     shuffle(cardList).forEach(createCard);
 }
 
-
+// Chamando a função "positioner" duas vezes
 function callPositioner(){
 	positioner()
 	positioner()
 }
 
+// Temporizador
+function countdown() {
+	function startTimer(duration, display) {
+		timer = duration, minutes, seconds;
+    	setInterval(function () {
+        minutes = parseInt(timer / 60, 10);
+        seconds = parseInt(timer % 60, 10);
 
-// estrelas
+        minutes = minutes < 10 ? "0" + minutes : minutes;
+        seconds = seconds < 10 ? "0" + seconds : seconds;
+
+        display.text(minutes + ":" + seconds);
+
+        if (timerFinish === false) {
+        	if (++timer < 0 && timerFinish) {
+                    timer = duration;
+                }
+            }
+    }, 1000);
+}
+
+	jQuery(function ($) {
+	    let sec = 0,
+	        display = $('#Timer');
+	    startTimer(sec, display);
+	});
+}
+
+// Logica para diminuir estrelas
 function countStars(){
 	if(moves === 8){
 		starsOut = 2
@@ -71,7 +101,15 @@ function countStars(){
 	}
 }
 
-
+// restart
+$('.restart').click( function() {
+	moves = minutes = seconds = timer = 0;
+	timerFinish = false;
+	$('.card').removeClass('shakeCorrect match shakeWrong');
+	$('ul.stars').children('li').find('*').removeClass('fa-star-o');
+	$('ul.stars').children('li').find('*').addClass('fa-star');
+	opened = [];
+})
 
 // verificando se a carta foi clicada
 function verifyClick() {
@@ -87,32 +125,6 @@ function verifyClick() {
 	})
 };
 
-// Temporizador
-function countdown() {
-	function startTimer(duration, display) {
-		timer = duration, minutes, seconds;
-    	setInterval(function () {
-        minutes = parseInt(timer / 60, 10);
-        seconds = parseInt(timer % 60, 10);
-
-        minutes = minutes < 10 ? "0" + minutes : minutes;
-        seconds = seconds < 10 ? "0" + seconds : seconds;
-
-        display.text(minutes + ":" + seconds);
-
-        if (timerFinish === false) {if (++timer < 0 && timerFinish) {
-                    timer = duration;
-                }}
-    }, 1000);
-}
-
-	jQuery(function ($) {
-	    let sec = 0,
-	        display = $('#Timer');
-	    startTimer(sec, display);
-	});
-}
-
 // verificando "match" de cartas
 function card_match() {
     if (opened[opened.length - 2].html() == opened[opened.length - 1].html()) {
@@ -121,11 +133,11 @@ function card_match() {
     	opened[opened.length - 2].addClass('shakeCorrect').delay(200).queue(function( next ){
 	    $(this).toggleClass('shakeWrong');
 	    next();
-});
+	});
     	opened[opened.length - 1].addClass('shakeCorrect').delay(200).queue(function( next ){
 	    $(this).toggleClass('shakeWrong');
 	    next();
-});
+	});
         opened[opened.length - 2].removeClass('open');
         opened[opened.length - 2].addClass('match');
         opened[opened.length - 1].removeClass('open');
@@ -136,59 +148,48 @@ function card_match() {
         opened[opened.length - 1].addClass('shakeWrong wrong').delay(200).queue(function( next ){
 	    $(this).toggleClass('shakeWrong');
 	    next();
-});
+	});
         opened[opened.length - 2].addClass('shakeWrong wrong').delay(200).queue(function( next ){
 	    $(this).toggleClass('shakeWrong');
 	    next();
-});
+	});
         opened[opened.length - 1].delay(250).queue(function( next ){
 	    $(this).toggleClass('open wrong');
 	    next();
-});
+	});
         opened[opened.length - 2].delay(250).queue(function( next ){
 	    $(this).toggleClass('open wrong');
 	    next();
 	    opened.pop();
     	opened.pop();
-});
+	});
 
     }
   	if (opened.length == 16) {
-  		$( ".container" ).css('opacity', '0')
-	  	timerFinish = true;
-		swal({
-		allowOutsideClick: false,
-		closeOnClickOutside: false,
-		allowEscapeKey: false,
-		title: "Congratulations, You Won!",
-		text: `In ${minutes + ":" + seconds}, ${moves} Moves and ${starsOut} Stars`,
-		icon: "success",
-		button: "Restart",
-	}).then(function(restart) {
-		if (restart) {
-			$( ".container" ).css('opacity', '1')
-			$('.card').remove()
-			callPositioner()
-			verifyClick()
-			moves = minutes = seconds = timer = 0;
-			starsOut = 3;
-			timerFinish = false;
-			$('.card').removeClass('shakeCorrect match shakeWrong');
-			$('ul.stars').children('li').find('*').removeClass('fa-star-o');
-			$('ul.stars').children('li').find('*').addClass('fa-star');
-			opened = [];
-		}
-	})
+	  		$( ".container" ).css('opacity', '0')
+		  	timerFinish = true;
+			swal({
+			allowOutsideClick: false,
+			closeOnClickOutside: false,
+			allowEscapeKey: false,
+			title: "Congratulations, You Won!",
+			text: `In ${minutes + ":" + seconds}, ${moves} Moves and ${starsOut} Stars`,
+			icon: "success",
+			button: "Restart",
+		}).then(function(restart) {
+			if (restart) {
+				$( ".container" ).css('opacity', '1')
+				$('.card').remove()
+				callPositioner()
+				verifyClick()
+				moves = minutes = seconds = timer = 0;
+				starsOut = 3;
+				timerFinish = false;
+				$('.card').removeClass('shakeCorrect match shakeWrong');
+				$('ul.stars').children('li').find('*').removeClass('fa-star-o');
+				$('ul.stars').children('li').find('*').addClass('fa-star');
+				opened = [];
+			}
+		})
+	}
 }
-}
-
-// restart
-
-$('.restart').click( function() {
-	moves = minutes = seconds = timer = 0;
-	timerFinish = false;
-	$('.card').removeClass('shakeCorrect match shakeWrong');
-	$('ul.stars').children('li').find('*').removeClass('fa-star-o');
-	$('ul.stars').children('li').find('*').addClass('fa-star');
-	opened = [];
-})
