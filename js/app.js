754e4336bd0f5cd0fbd322b18d35e1c01113e631
Marco Rezende <marcorezende@outlook.com.br>
2018-11-26 20:14:30 -0200

    Colocando o codigo javascript em conformidade com o guia de estilo

--- js/app.js
+++ js/app.js
@@ -1,31 +1,31 @@
-const cardList = ["fa-birthday-cake", "fa-send", "fa-anchor", "fa-bolt", "fa-cube", "fa-leaf", "fa-bicycle", "fa-bomb"];
+const cardList = ['fa-birthday-cake', 'fa-send', 'fa-anchor', 'fa-bolt', 'fa-cube', 'fa-leaf', 'fa-bicycle', 'fa-bomb'];
 let timer, duration, seconds, minutes;
 let moves = 0, starsOut = 3;
 let timerFinish = false
 let toggle = true;
-let opened = []
+let opened = [];
 
 
 // Iniciador
-start()
+start();
 
 // Função que executa o modal inicial e outras funções para dar inicio ao jogo
 function start() {
 	swal({
 		title: "Welcome to the 'Matching Game'",
-		buttons: "START",
+		buttons: 'START',
 		allowOutsideClick: false,
 		closeOnClickOutside: false,
 		allowEscapeKey: false,
 	}).then((normalMode) => {
 		if (normalMode) {
-			$( ".container" ).css('opacity', '1')
-			callPositioner()
-			verifyClick()
-			countdown()
-	  	}
-	})
-};
+			$( '.container' ).css('opacity', '1');
+			callPositioner();
+			verifyClick();
+			countdown();
+	  	};
+	});
+}
 
 // Função que randomiza os cards
 function shuffle(array) {
@@ -36,13 +36,13 @@ function shuffle(array) {
         temporaryValue = array[currentIndex];
         array[currentIndex] = array[randomIndex];
         array[randomIndex] = temporaryValue;
-    }
+    };
     return array;
 }
 
-// Criando cards a partir de um icone e o posicionando como ultimo filho do "grid"
+// Criando cards a partir de um icone e o posicionando como ultimo filho do 'grid'
 function createCard(cardClass) {
-    $("ul.grid").append(`<li class="card"><i class="fa ${cardClass}"></i></li>`);
+    $('ul.grid').append(`<li class='card'><i class='fa ${cardClass}'></i></li>`);
 }
 
 // Passando para função createCard um novo icone, para que se crie um novo card
@@ -50,10 +50,10 @@ function positioner(){
     shuffle(cardList).forEach(createCard);
 }
 
-// Chamando a função "positioner" duas vezes
+// Chamando a função 'positioner' duas vezes
 function callPositioner(){
-	positioner()
-	positioner()
+	positioner();
+	positioner();
 }
 
 // Temporizador
@@ -64,19 +64,18 @@ function countdown() {
         minutes = parseInt(timer / 60, 10);
         seconds = parseInt(timer % 60, 10);
 
-        minutes = minutes < 10 ? "0" + minutes : minutes;
-        seconds = seconds < 10 ? "0" + seconds : seconds;
+        minutes = minutes < 10 ? '0' + minutes : minutes;
+        seconds = seconds < 10 ? '0' + seconds : seconds;
 
-        display.text(minutes + ":" + seconds);
+        display.text(minutes + ':' + seconds);
 
         if (timerFinish === false) {
         	if (++timer < 0 && timerFinish) {
                     timer = duration;
-                }
-            }
+                };
+            };
     }, 1000);
-}
-
+};
 	jQuery(function ($) {
 	    let sec = 0,
 	        display = $('#Timer');
@@ -87,16 +86,17 @@ function countdown() {
 // Logica para diminuir estrelas
 function countStars(){
 	if(moves === 8){
-		starsOut = 2
+		starsOut = 2;
 		$('#Star3').addClass('fa-star-o');
 		$('#Star3').removeClass('fa-star');
-	}
+	};
 	if(moves === 24){
-		starsOut = 1
+		starsOut = 1;
 		$('#Star2').addClass('fa-star-o');
 		$('#Star2').removeClass('fa-star');
-	} if(moves === 34){
-		starsOut = 0
+	};
+	if(moves === 34){
+		starsOut = 0;
 		$('#Star1').addClass('fa-star-o');
 		$('#Star1').removeClass('fa-star');
 	}
@@ -110,28 +110,28 @@ $('.restart').click( function() {
 	$('ul.stars').children('li').find('*').removeClass('fa-star-o');
 	$('ul.stars').children('li').find('*').addClass('fa-star');
 	opened = [];
-})
+});
 
 // verificando se a carta foi clicada
 function verifyClick() {
 	$('.card').click(function() {
 		if(!($(this).hasClass('open') || $(this).hasClass('match')) && $('.open').length < 2){
-			countStars()
+			countStars();
 			$(this).addClass('open');
 		    opened.push($(this));
 		    if (opened.length % 2 == 0) {
 		        setTimeout(card_match, 500);
-		    }
-		}
-	})
-};
+		    };
+		};
+	});
+}
 
-// verificando "match" de cartas
+// verificando 'match' de cartas
 function card_match() {
     if (opened[opened.length - 2].html() == opened[opened.length - 1].html()) {
     	moves++;
 		$('.moves').text(moves);
-    	opened[opened.length - 2].addClass('shakeCorrect')
+    	opened[opened.length - 2].addClass('shakeCorrect');
     	opened[opened.length - 1].addClass('shakeCorrect');
         opened[opened.length - 2].removeClass('open');
         opened[opened.length - 2].addClass('match');
@@ -159,24 +159,24 @@ function card_match() {
     	opened.pop();
 	});
 
-    }
+    };
   	if (opened.length == 16) {
-	  		$( ".container" ).css('opacity', '0')
+	  		$( '.container' ).css('opacity', '0')
 		  	timerFinish = true;
 			swal({
 			allowOutsideClick: false,
 			closeOnClickOutside: false,
 			allowEscapeKey: false,
-			title: "Congratulations, You Won!",
-			text: `In ${minutes + ":" + seconds}, ${moves} Moves and ${starsOut} Stars`,
-			icon: "success",
-			button: "Restart",
+			title: 'Congratulations, You Won!',
+			text: `In ${minutes + ':' + seconds}, ${moves} Moves and ${starsOut} Stars`,
+			icon: 'success',
+			button: 'Restart',
 		}).then(function(restart) {
 			if (restart) {
-				$( ".container" ).css('opacity', '1')
-				$('.card').remove()
-				callPositioner()
-				verifyClick()
+				$( '.container' ).css('opacity', '1');
+				$('.card').remove();
+				callPositioner();
+				verifyClick();
 				moves = minutes = seconds = timer = 0;
 				starsOut = 3;
 				timerFinish = false;
@@ -184,8 +184,8 @@ function card_match() {
 				$('ul.stars').children('li').find('*').removeClass('fa-star-o');
 				$('ul.stars').children('li').find('*').addClass('fa-star');
 				opened = [];
-			}
-		})
-	}
+			};
+		});
+	};
 }
 
